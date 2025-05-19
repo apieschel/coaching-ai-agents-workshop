50b4da323badc4ec5584c5b0a027ca810739346b
rondagdag <rlyle78@gmail.com>
2025-05-18 23:40:25 -0700

    fix: import END constant from langgraph for workflow graph completion

--- demo3/coagents-starter/agent-js/src/agent.ts
+++ demo3/coagents-starter/agent-js/src/agent.ts
@@ -8,7 +8,7 @@ import { RunnableConfig } from "@langchain/core/runnables";
 import { tool } from "@langchain/core/tools";
 import { ToolNode } from "@langchain/langgraph/prebuilt";
 import { AIMessage, SystemMessage } from "@langchain/core/messages";
-import { MemorySaver, START, StateGraph } from "@langchain/langgraph";
+import { MemorySaver, START, END, StateGraph } from "@langchain/langgraph";
 import { Annotation } from "@langchain/langgraph";
 import { ChatOpenAI } from "@langchain/openai";
 
